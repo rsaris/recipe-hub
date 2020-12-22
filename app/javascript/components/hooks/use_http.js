650dae96f7cf80e40e81d4bb358758af1a2defb8
Bob Saris <bob@bobsaris.com>
2020-12-21 23:43:30 -0500

    Add tooling for processing JSONAPI in JS.

---
+++ app/javascript/components/hooks/use_http.js
@@ -0,0 +1,42 @@
+import React, { useCallback, useMemo, useState } from 'react';
+
+import { JsonApiDataStore } from 'jsonapi-datastore';
+
+import {
+  httpDelete as rawHttpDelete,
+  httpGet as rawHttpGet,
+  httpPatch as rawHttpPatch,
+  httpPost as rawHttpPost,
+  httpPut as rawHttpPut,
+} from 'lib/http';
+
+function createHttpMethod(setLoading, httpMethod, store) {
+  return async (path, body) => {
+    setLoading(true);
+    const response = await httpMethod(path, body);
+    setLoading(false);
+    return store.sync(response);
+  };
+}
+
+export default function useHttp() {
+  const [loading, setLoading] = useState(false);
+
+  const store = useMemo(() => new JsonApiDataStore(), []);
+
+  const httpDelete = useCallback(createHttpMethod(setLoading, rawHttpDelete, store), [store]);
+  const httpGet = useCallback(createHttpMethod(setLoading, rawHttpGet, store), [store]);
+  const httpPatch = useCallback(createHttpMethod(setLoading, rawHttpPatch, store), [store]);
+  const httpPost = useCallback(createHttpMethod(setLoading, rawHttpPost, store), [store]);
+  const httpPut = useCallback(createHttpMethod(setLoading, rawHttpPut, store), [store]);
+
+  return {
+    loading,
+    httpDelete,
+    httpGet,
+    httpPatch,
+    httpPost,
+    httpPut,
+    store,
+  }
+}
---
+++ app/javascript/lib/http.js
@@ -0,0 +1,75 @@
+const statuses = {
+  UNKNOWN: -1,
+  OK: 200,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  UNPROCESSABLE_ENTITY: 422,
+  INTERNAL_SERVER_ERROR: 500,
+  SERVICE_UNAVAILABLE: 503,
+};
+
+async function callFetch(path, request) {
+  const response = await fetch(path, request);
+
+  if (response.status === statuses.NO_CONTENT) { return null; }
+
+  if (response.ok) { return response.json(); }
+
+  throw response.statusText;
+}
+
+function getCSRFParam() {
+  return document.querySelector('meta[name="csrf-param"]').getAttribute('content');
+}
+
+function getCSRFToken() {
+  return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+}
+
+function buildRequest(method, body) {
+  return {
+    body: JSON.stringify(body),
+    cache: 'default',
+    credentials: 'same-origin',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHTTPRequest',
+      'X-CSRF-Token': getCSRFToken(),
+    },
+    method,
+  };
+}
+
+function httpDelete(path, body) {
+  return callFetch(path, buildRequest('DELETE', body));
+}
+
+function httpGet(path) {
+  return callFetch(path, buildRequest('GET'));
+}
+
+function httpPatch(path, body) {
+  return callFetch(path, buildRequest('PATCH', body));
+}
+
+function httpPost(path, body) {
+  return callFetch(path, buildRequest('POST', body));
+}
+
+function httpPut(path, body) {
+  return callFetch(path, buildRequest('PUT', body));
+}
+
+export {
+  getCSRFParam,
+  getCSRFToken,
+  buildRequest,
+  httpDelete,
+  httpGet,
+  httpPatch,
+  httpPost,
+  httpPut,
+}
